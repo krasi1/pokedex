9ce8d565d03065662299f275c120263dab9989b9
krasi1 <krissi.petrov@gmail.com>
2022-11-09 23:15:02 +0200

    adding list of mons

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,16 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const ul = document.querySelector("ul");
+
+  const completeList = document.querySelector("ul");
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+    .then((response) => response.json())
+    .then((data) => {for (let i = 0; i < data.results.length; i++) 
+    {
+      let listItem = document.createElement("li");
+      listItem.innerText = data.results[i].name;
+      completeList.appendChild(listItem);
+    }}
+    );
+
 });
